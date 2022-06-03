d6459c989c7136cfb28b19b37a8adfab961a1119
favour-22 <105045421+favour-22@users.noreply.github.com>
2022-06-03 23:23:23 +0100

    Update 2-print_alphabet.c
    lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
+
 /**
- * main-print all the alphabet in lowerr case 
+ * main - Prints the alphabet in lowercase.
  *
- * Return -always 0
+ * Return: Always 0.
  */
 int main(void)
 {
-	 char letter;
+	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
- 		putchar(letter);
-	
+		putchar(letter);
+
 	putchar('\n');
 
 	return (0);
 }
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+
