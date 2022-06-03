266f7c8f45c1d01135df29a99be8e63e999efc76
favour-22 <105045421+favour-22@users.noreply.github.com>
2022-06-04 00:53:01 +0100

    Update 102-print_comb5.c
    coding so cool

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,27 +8,27 @@
  */
 int main(void)
 {
-        int num1, num2;
+	int num1, num2;
 
-        for (num1 = 0; num1 <= 98; num1++)
-        {
-                for (num2 = num1 + 1; num2 <= 99; num2++)
-                {
-                        putchar((num1 / 10) + '0');
-                        putchar((num1 % 10) + '0');
-                        putchar(' ');
-                        putchar((num2 / 10) + '0');
-                        putchar((num2 % 10) + '0');
+	for (num1 = 0; num1 <= 98; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-                        if (num1 == 98 && num2 == 99)
-                                continue;
+			if (num1 == 98 && num2 == 99)
+				continue;
 
-                        putchar(',');
-                        putchar(' ');
-                }
-        }
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
