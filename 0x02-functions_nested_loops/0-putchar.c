7531376a7ea255e379664f4a09dca923da6f2475
favour-22 <anointed657@gmail.com>
2022-06-07 13:58:51 -0700

    printing _pitchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - print "-putchar " followed a new line .
+ * Description :Do not use standard libaries.
+ * Return : 0
+ */
+
+int main(void);
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	_putchar("\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Coding about nested loop
