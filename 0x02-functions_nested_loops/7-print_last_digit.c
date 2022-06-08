10400ac44e36a3c7ba97d9249b035f5363b27b57
favour-22 <105045421+favour-22@users.noreply.github.com>
2022-06-08 01:09:18 +0100

    Update 7-print_last_digit.c
    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ * @x: int type number
+ * Return: return value of last digit
+ */
+
+int print_last_digit(int x)
+{
+	int y;
+
+	if (x < 0)
+	{
+		y = -1 * (x % 10);
+		_putchar(y + '0');
+		return (y);
+	}
+	else
+	{
+		y = x % 10;
+		_putchar(y + '0');
+		return (y);
+	}
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
