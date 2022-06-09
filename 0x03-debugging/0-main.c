8cf0849690f1bdfd78b8ce8839e3e257d3d2a510
favour-22 <105045421+favour-22@users.noreply.github.com>
2022-06-09 10:02:45 +0100

    Update 0-main.c
    positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- * Rerurn -0
+ * Return: 0
  */
 
 int main(void)
@@ -12,5 +12,5 @@ int main(void)
 	i = 0;
 	positive_or_negative(i);
 
-	return	(0);
-}	
+	return (0);
+}
