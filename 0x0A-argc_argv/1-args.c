2934831ec4a56ab526f1ffcdd9b19936594a7e57
favour-22 <anointed657@gmail.com>
2022-07-07 06:03:57 -0700

    Updated README

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * main - print the number of arguments passed to the program
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: Always 0 (Success)
+ * Return: Always 0 (Succes0)
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
